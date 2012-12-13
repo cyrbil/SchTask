a6dc8bde63ddf15e8c6d380efbfb5b38e0f0a6e2
Cyril DEMINGEON <cyrbil@hotmail.fr>
2012-12-13 14:45:44 -0500

    Initial project commit
    Raw final version; full project with exe.

---
+++ SchTask/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchTask
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                // vars
+                string account = null, system = null, user = null, password = null, taskFilter = null, filters = null, returnFormat = "readable";
+                string[] filtersArray = null;
+                bool verbose = false;
+                string[] validArgs = { "-a", "-s", "-u", "-p", "-t", "-f", "-r"};
+                for (int i = 0; i < args.Length; i++)
+                {
+                    // c'est un argument, on regarde la suite.
+                    if (validArgs.Contains(args[i]))
+                    {
+                        if (validArgs.Contains(args[i + 1])) // si c'est un arg, error
+                            throw new Exception("L'argument " + args[i] + " attend comme paramètre une chaine de caractères");
+                        // argument valide, on enregistre la string.
+                        switch (args[i].ToLower())
+                        {
+                            case "-a": account = args[++i].Trim(); break;
+                            case "-s": system = args[++i].Trim(); break;
+                            case "-u": user = args[++i].Trim(); break;
+                            case "-p": password = args[++i].Trim(); break;
+                            case "-t": taskFilter = args[++i].Trim(); break;
+                            case "-f": filters = args[++i].Trim(); break;
+                            case "-r": returnFormat = args[++i].Trim(); break;
+                        }
+                    }
+                    else if (args[i].Equals("-v"))// si c'est verbose, on active le mode
+                        verbose = true;
+                    else if (args[i].Equals("-h") || args[i].Equals("/?") || args[i].Equals("--help")) // si c'est help on display et quitte
+                        return displayHelp();
+
+                }
+                if (!String.IsNullOrEmpty(filters)) // validation des filtres
+                {
+                    string[] validFilters = { 
+                        "name", "trigger", "nextruntime", "enable", "lastrun",
+                        "lastruntime", "lastrunreturn", "path",
+                        "status", "command", "author", "description",
+                        "executiontimelimit", "priority"
+                    };
+                    filtersArray = filters.Split(",".ToCharArray());
+                    if(!filtersArray.All(str => validFilters.Contains(str.ToLower()))) {
+                        throw new Exception("Bad filters. Valid filters are:\n\t\tname,trigger,nextruntime,enable,\n\t\tlastrun,lastruntime,lastrunreturn,path,\n\t\tstatus,command,author,description,\n\t\texecutiontimelimit,priority");
+                    }
+                }
+
+                // arguments are ok and filtered, let's do it !
+                new Scheduler(account, system, user, password, taskFilter, filtersArray, verbose, returnFormat);
+            }
+            catch (Exception e) {
+                Console.WriteLine("An error occured: " + e.GetType() + "\n\t" + e.Message);
+                Console.WriteLine("\nStack:\n" + e.StackTrace);
+                return 2;
+            }
+            return 0;
+        }
+
+        private static int displayHelp()
+        {
+            Console.WriteLine("usage: ");
+            Console.WriteLine("  -a    account           Spécifie l'account domaine pour la machine distante");
+            Console.WriteLine("  -s    système           Spécifie le système distant auquel se connecter.");
+            Console.WriteLine("  -u    utilisateur       Spécifie le contexte de l'utilisateur");
+            Console.WriteLine("                          que schtasks.exe doit exécuter.");
+            Console.WriteLine("  -p    [mot_passe]       Spécifie le mot de passe pour le contexte");
+            Console.WriteLine("                          utilisateur donné. Il est demandé s'il est omis.");
+            Console.WriteLine("  -t    task name         Filtre les resultat pour une tache");
+            Console.WriteLine("  -v                      Affiche tout les détails sur le(s) tâche(s) demandée(s)");
+            Console.WriteLine("  -f    filter[,f[...]]   Filtre les résultats en spécifiant la/les colonne(s) souhaitée(s)");
+            Console.WriteLine("  -r    json/csv/readable Retourne les données dans le format choisi (readable = default)");
+            Console.WriteLine("  -? / --help             Donne cette aide");
+            return 1;
+        }
+    }
+}
---
+++ SchTask/Scheduler.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32.TaskScheduler;
+
+namespace SchTask
+{
+    class Scheduler
+    {
+        private String account;
+        private String system;
+        private String user;
+        private String password;
+        private String taskFilter;
+        private String[] filters = new String[] {};
+        private String returnFormat;
+        private Boolean verbose;
+        private TaskService ts;
+        private Task[] tasks;
+        private List<Dictionary<String, String>> taskList = new List<Dictionary<String, String>>();
+        
+
+        public Scheduler(string account = null, string system = null, string user = null, string password = null, string taskFilter = null, string[] filters = null, bool verbose = false, string returnFormat = "readable")
+        {
+            this.account = account;
+            this.system = system;
+            this.user = user;
+            this.password = password;
+            this.taskFilter = (String.IsNullOrEmpty(taskFilter)) ? ".*" : "^" + taskFilter + "$";
+            this.filters = filters;
+            this.verbose = verbose;
+            this.returnFormat = returnFormat;
+
+            this.getSchedule();
+        }
+
+        private void getSchedule()
+        {
+            using (this.ts = new TaskService(system, user, account, password))
+            {
+                this.tasks = ts.FindAllTasks(new System.Text.RegularExpressions.Regex(taskFilter), true);
+                foreach(Task task in this.tasks)
+                    taskList.Add(filter(task));
+                this.display();
+            }
+        }
+
+        private Dictionary<String, String> filter(Task task)
+        {
+            /* Available filters: 
+             *      name
+             *      trigger (definition.triggers)
+             *      nextRunTime
+             *      enable
+             *      lastRunTime
+             *      lastRunReturn
+             *      lastRun (lastRunTime + lastRunReturn)
+             *      path
+             *      status
+             *      command (definition.action)
+             *      author (definition.registrationinfo.author
+             *      description (definition.registrationinfo.descrition)
+             *      executionTimeLimit (definition.settings.executionTimeLimit)
+             *      priority (definition.settings.priority)
+             */
+            Dictionary<String, String> r = new Dictionary<String, String>();
+
+
+            if (this.filters == null)
+            {// default filter:
+                if (this.verbose)
+                    this.filters = new string[] {
+                        "name", "trigger", "nextruntime", "enable", "lastRun",
+                        "path", "status", "command", "author",
+                        "description", "executiontimelimit", "priority"
+                    };
+                else
+                    this.filters = new string[] {
+                        "name", "description", "trigger", "enable"
+                    };
+            }
+
+            foreach (String filter in this.filters)
+            {
+                switch(filter.ToLower()) {
+                    case "name":
+                        r.Add(filter.ToLower(), task.Name);
+                        break;
+                    case "trigger":
+                        r.Add(filter.ToLower(), task.Definition.Triggers.ToString());
+                        break;
+                    case "lastrun":
+                        r.Add(filter.ToLower(), task.LastRunTime.ToString() + " (Return: " + task.LastTaskResult.ToString() + ")");
+                        break;
+                    case "enable":
+                        r.Add(filter.ToLower(), (task.Enabled) ? "true" : "false");
+                        break;
+                    case "nextruntime":
+                        r.Add(filter.ToLower(), task.LastRunTime.ToString());
+                        break;
+                    case "lastrunreturn":
+                        r.Add(filter.ToLower(), task.LastTaskResult.ToString());
+                        break;
+                    case "path":
+                        r.Add(filter.ToLower(), task.Path);
+                        break;
+                    case "status":
+                        r.Add(filter.ToLower(), task.State.ToString());
+                        break;
+                    case "command":
+                        r.Add(filter.ToLower(), task.Definition.Actions.ToString());
+                        break;
+                    case "author":
+                        r.Add(filter.ToLower(), task.Definition.RegistrationInfo.Author);
+                        break;
+                    case "description":
+                        r.Add(filter.ToLower(), task.Definition.RegistrationInfo.Description);
+                        break;
+                    case "executiontimelimit":
+                        r.Add(filter.ToLower(), task.Definition.Settings.ExecutionTimeLimit.ToString());
+                        break;
+                    case "priority":
+                        r.Add(filter.ToLower(), task.Definition.Settings.Priority.ToString());
+                        break;
+                }
+            }
+            return r;
+        }
+
+        private void display()
+        {
+            if (this.taskList.Count == 0)
+            {
+                throw new KeyNotFoundException("No Task Found for this filter.");
+            }
+            String[] separators = new String[] { "", "--------------------------\n", "", 
+                                                    /* KEY */ " => ", /* VALUE */ "\n\n", 
+                                                 "", "", null };
+            
+            switch (returnFormat.ToLower())
+            {
+                case "csv":
+                    separators = new String[] { "", "", "\"", ",", "\n", "", "", "[\\\\\"]" };
+                break;
+                case "json":
+                separators = new String[] { "[", "{", "\"", ":", ",", "}", "]", "[\\\\\"]" };
+                break;
+            }
+
+            if(this.taskList.Count != 1) Console.Write(separators[0]);
+            foreach(Dictionary<String, String> task in this.taskList)
+            {
+                Console.Write(separators[1]);
+                foreach (KeyValuePair<String, String> property in task)
+                {
+                    Console.Write(separators[2] + property.Key + separators[2] + separators[3] + separators[2]);
+                    if(!String.IsNullOrEmpty(separators[7]))
+                        Console.Write(System.Text.RegularExpressions.Regex.Replace((property.Value == null) ? "" : property.Value, separators[7], "\\$0"));
+                    else
+                        Console.Write(property.Value);
+                    Console.Write(separators[2]);
+                    if (!property.Equals(task.Last())) Console.Write(separators[4]);
+                }
+                Console.Write(separators[5]);
+                if (!task.Equals(this.taskList.Last())) Console.Write(separators[4]);
+            }
+            if (this.taskList.Count != 1) Console.Write(separators[6]);
+        }
+    }
+}
